984964714349841ee0021364717420cecb265a99
Bilel Issaoui <7354810-bileli@users.noreply.gitlab.com>
2022-12-20 21:57:27 +0000

    Update file sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -14,3 +14,4 @@ def call(String buildStatus = 'STARTED') {
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
 
     slackSend(color: color, message: msg)
+}
